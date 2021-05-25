4d2c40eaa7ad079e6fd23bfa24274b349b0db0ce
Cláudio Luis Alves Monteiro <claudiomonteiro.neurotech@sulamerica.com.br>
2021-05-25 07:32:45 -0300

    update_plots

--- results/experiments_visualization.R
+++ results/experiments_visualization.R
@@ -6,13 +6,16 @@ library(ggplot2)
 library(ggpubr)
 library(stringr)
 library(viridis)
+library(data.table)
 
 
 
 
-#==============
-# xor
-#=============
+#============================
+#         xor lite
+#============================
+
+##----- average search
 
 data = read_csv('experiment_xor.csv')
 
@@ -31,6 +34,36 @@ ggplot(data, aes(x=phase_strategy, y=avg_error, fill=phase_strategy)) +
 
 ggsave('results/xor-binary-models.png', height = 4, width = 7, units = 'in')  
 
+###----- epoch error
+
+xornobias = read_csv('results/epoch_error_xorlite_nobias.csv')
+xornobias <- melt(setDT(xornobias), id.vars = c("X1","epoch"), variable.name = "model")
+p1 = ggplot(xornobias, aes(x=epoch, y=value, group=model, color=model)) +
+  geom_line(size=1) +
+  scale_color_viridis(discrete = TRUE) +
+  labs(x='Epoch', y="Epoch Error", title='Without Bias')+
+  theme_minimal()+
+  theme(legend.title = element_blank(), 
+        legend.text = element_text(size=11))+
+  guides(color = guide_legend(override.aes = list(size = 4) ) )
+
+p1
+
+xorbias = read_csv('results/epoch_error_xorlite_bias.csv')
+xorbias <- melt(setDT(xorbias), id.vars = c("X1","epoch"), variable.name = "model")
+p2 = ggplot(xorbias, aes(x=epoch, y=value, group=model, color=model)) +
+  geom_line(size=1) +
+  scale_color_viridis(discrete = TRUE) +
+  labs(x='Epoch', y="Epoch Error", title='With Bias')+
+  theme_minimal() +
+  theme(legend.title = element_blank(), 
+        legend.text = element_text(size=11))+
+  guides(color = guide_legend(override.aes = list(size = 4)) )
+
+p2
+
+ggarrange(p1, p2, ncol = 1, nrow = 2, common.legend = T, legend='bottom')
+ggsave('results/xor-epoch.png', height = 5, width = 7, units = 'in')  
 
 #==============
 # xor real
@@ -42,6 +75,7 @@ data$phase_strategy = paste0("phase-encoding\n", data$phase_strategy)
 
 data$phase_strategy[data$model == 'HSGS'] = 'HSGS'
 
+aggregate(data$avg_error, by=list(data$phase_strategy), mean)
 
 ggplot(data, aes(x=phase_strategy, y=avg_error, fill=phase_strategy)) + 
   geom_boxplot()+
